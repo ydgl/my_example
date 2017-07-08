dd7d2ac5405bf31a5c47348c4e293172e975450d
ydgl <denisgilles.laurent@gmail.com>
2017-07-08 06:36:47 +0200

    ajout du test tomcat

---
+++ run_tomcat_2/src/main/webapp/index2.jsp
@@ -0,0 +1,7 @@
+<%@ taglib prefix="mt" uri="WEB-INF/ydgl.tld"%>
+<html>
+<body>
+<h2>Hello World!</h2>
+<h2><mt:MyTag/></h2>
+</body>
+</html>
---
+++ testtcat/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        out.println ("hello world");
+	// write your code here
+    }
+}
